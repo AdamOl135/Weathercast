6b691bc08fe4fdbac15083e17b5e75bc3b151cb6
adam <adamleeoroszi@gmail.com>
2026-07-26 20:31:36 +0200

    fixed bug from todo, st.write(logo) produced text from properties, fix by handing over st methods directly to variables instead of indirectly handing over variables to methods

--- src/main.py
+++ src/main.py
@@ -4,12 +4,9 @@ import streamlit as st
 with (st.container(border = True,horizontal=True, horizontal_alignment = "distribute")):
 
     logo = st.image("assets/PCLOUDY1.png")
-    name = "Weathercast"
-    search = "search"
+    name = st.write("Weathercast")
+    search = st.write("search")
 
-    st.write(logo)
-    st.write(name)
-    st.write(search)
 
 
 #todo : bugfix for logo (lots of code shows up out of nowhere)
