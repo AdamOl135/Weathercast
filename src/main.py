d8d49791be6cdc375581cc441d77b8f5065c361d
Adam <adamleeoroszi@gmail.com>
2026-07-27 01:45:38 +0200

    main.py : added sidebar temporarily, removed unecessary code, cleaned up look of search, formatted title

--- src/main.py
+++ src/main.py
@@ -1,19 +1,20 @@
 import streamlit as st
 
-st.title("title")
-
-
-# top row with logo, app name and search function
-with (st.container(border = True,horizontal=True, horizontal_alignment = "distribute",width="stretch")):
-
-
-
+with st.sidebar:
     logo = st.image("assets/PCLOUDY1.png",width= 30)
-    logo.space = ("stretch")
-    name = st.write("Weathercast")
+
+
+
+#top row with logo, app name and search function
+with st.container(horizontal=True, horizontal_alignment = "distribute",width="stretch"):
+
+    #logo & Title
+    logo = st.image("assets/PCLOUDY1.png",width= 30)
+    name = st.markdown("**Weathercast**")
 
     #spacing between elements
     st.space("stretch")
 
     #search button for city with max input as safety precaution
-    search = st.text_input(label="search for city",max_chars=100)
+    search = st.text_input(label = "placeholder", label_visibility="collapsed", max_chars=100,
+                           placeholder= "Search for City")
